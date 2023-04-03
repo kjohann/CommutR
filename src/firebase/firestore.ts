190e1911bbfdfa42bb778cc2b8857b2be9444987
kjohann <kristian.johannessen@bekk.no>
2023-04-03 13:02:28 +0200

    Firebase setup

---
+++ src/firebase/firestore.ts
@@ -0,0 +1,17 @@
+
+import { getFirestore, connectFirestoreEmulator, addDoc, collection, WithFieldValue, DocumentData, Firestore } from "firebase/firestore";
+import { app } from "./init";
+
+let db: Firestore;
+if (import.meta.env.VITE_USEEMULATORS) {
+  console.warn("VITE_USEEMULATORS is set to true. This should only happen locally...")
+  db = getFirestore();
+  connectFirestoreEmulator(db, 'localhost', 8080);
+} else {
+  db = getFirestore(app);
+}
+
+export const addDocument = async <T extends WithFieldValue<DocumentData>>(collectionName: string, doc: T) => {
+  const collectionRef = collection(db, collectionName);
+  return await addDoc(collectionRef, doc);
+}
---
+++ src/firebase/init.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUGxjY5jLnUZ74t2SmxoiU1mEeJOYYpq0",
+  authDomain: "commutr-bdb37.firebaseapp.com",
+  projectId: "commutr-bdb37",
+  storageBucket: "commutr-bdb37.appspot.com",
+  messagingSenderId: "326274075279",
+  appId: "1:326274075279:web:d4c43bc6cda57198d37efa"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
